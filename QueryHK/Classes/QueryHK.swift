19be3cc68c3a1756b64d5084a289f68e35c7da45
twoolf <twoolf@jhu.edu>
2016-07-26 22:13:51 -0400

    mods to support initial query for testing

--- QueryHK/Classes/QueryHK.swift
+++ QueryHK/Classes/QueryHK.swift
@@ -1,3 +1,20 @@
+import HealthKit
+
+    public func saveWorkout(startDate: NSDate, endDate: NSDate, activityType: HKWorkoutActivityType, distance: Double, distanceUnit: HKUnit, kiloCalories: Double, metadata:NSDictionary, completion: ( (Bool, NSError!) -> Void)!)
+    {
+        log.debug("Saving workout \(startDate) \(endDate)")
+
+        let distanceQuantity = HKQuantity(unit: distanceUnit, doubleValue: distance)
+        let caloriesQuantity = HKQuantity(unit: HKUnit.kilocalorieUnit(), doubleValue: kiloCalories)
+
+        let workout = HKWorkout(activityType: activityType, startDate: startDate, endDate: endDate, duration: abs(endDate.timeIntervalSinceDate(startDate)), totalEnergyBurned: caloriesQuantity, totalDistance: distanceQuantity, metadata: metadata  as! [String:String])
+
+        healthKitStore.saveObject(workout, withCompletion: { (success, error) -> Void in
+            if( error != nil  ) { completion(success,error) }
+            else { completion(success,nil) }
+        })
+    }
+
     public func saveRunningWorkout(startDate: NSDate, endDate: NSDate, distance:Double, distanceUnit: HKUnit, kiloCalories: Double, metadata: NSDictionary, completion: ( (Bool, NSError!) -> Void)!)
     {
         saveWorkout(startDate, endDate: endDate, activityType: HKWorkoutActivityType.Running, distance: distance, distanceUnit: distanceUnit, kiloCalories: kiloCalories, metadata: metadata, completion: completion)
